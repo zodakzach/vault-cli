85b4e48875c8639286253d5ce7523f6fa7fa772a
zodakzach <zachary.cervenka@gmail.com>
2024-09-25 19:38:25 -0400

    update db path in main

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
     "log"
+    "os"
     "data-manager/cmd"
     "data-manager/database"
 )
 
 func main() {
+    // Get the database path from the environment variable
+    dbPath := os.Getenv("SQLITE_DB")
+    if dbPath == "" {
+        log.Fatal("SQLITE_DB environment variable is not set")
+    }
+
     // Initialize the database
-    if err := database.InitDB("vault.db"); err != nil {
+    if err := database.InitDB(dbPath); err != nil {
         log.Fatalf("Could not initialize the database: %v", err)
     }
 
