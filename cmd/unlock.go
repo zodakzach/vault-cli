2c09c5182c0cb535d6ea3ca12317144eb045025c
zodakzach <zachary.cervenka@gmail.com>
2024-10-03 23:38:53 -0400

    get rid of password flag for unlock now prompts user

--- cmd/unlock.go
+++ cmd/unlock.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
-	db "vault-cli/database"
-	"vault-cli/vault" // Make sure to import the vault package for timer control
+	db "vault-cli/database" 
+	"vault-cli/vault"
 	"fmt"
+	"log"
+	"golang.org/x/term" // This package allows for hidden password input
+	"syscall"
 	"github.com/spf13/cobra"
 )
 
@@ -12,12 +15,22 @@ var unlockCmd = &cobra.Command{
 	Short: "Unlock the vault",
 	Long:  `Unlock the vault by providing the master password.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		password, _ := cmd.Flags().GetString("password")
-
+		// Check if the master password is set
 		if err := db.CheckMasterPasswordSet(); err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
+
+		// Prompt for the password and hide input
+		fmt.Print("Enter master password: ")
+		passwordBytes, err := term.ReadPassword(int(syscall.Stdin))
+		fmt.Println() // To move to the next line after password input
+
+		if err != nil {
+			log.Fatal("Error reading password:", err)
+		}
+
+		password := string(passwordBytes)
 
 		// Verify the master password
 		valid, err := db.VerifyMasterPassword(password)
@@ -30,15 +43,17 @@ var unlockCmd = &cobra.Command{
 			return
 		}
 
+		// Unlock the vault
 		err = vault.UnlockVault()
 		if err != nil {
 			fmt.Println("Error unlocking the vault:", err)
 			return
 		}
+
+		fmt.Println("Vault unlocked successfully!")
 	},
 }
 
 func init() {
-	unlockCmd.Flags().StringP("password", "p", "", "Master password (required)")
-	unlockCmd.MarkFlagRequired("password")
+	// No need for a password flag anymore since we're prompting interactively
 }
